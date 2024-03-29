72efc63b0c2a21589be4c3539090a129afa7b595
Ketan Kadam <ketank39@gmail.com>
2024-03-30 01:12:21 +0530

    icon and name updated

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Three Frames',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
